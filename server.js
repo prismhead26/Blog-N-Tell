c6851579d6b9d24a1e2a5156e66a158fd82c99b6
Aiden Wahed <prismhead26@gmail.com>
2024-04-05 23:19:37 -0600

    comments updated, bugs fixed, styling, and modal style testing

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
-    console.log(`Patient Portal Listening on port http://localhost:${PORT}`)
+    console.log(`Blog'N Tell Listening on port http://localhost:${PORT}`)
   );
 });
